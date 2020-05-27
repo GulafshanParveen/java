684be9070bb0fbc2466c2b31897afc5446adb859
Gulafshan Parveen <64768393+GulafshanParveen@users.noreply.github.com>
2020-05-28 04:53:24 +0530

    Additon of String

---
+++ AddString.java
@@ -0,0 +1,10 @@
+public class AddString{
+	public static void main(String args[]){
+	String comment,gulafsha,is,best;
+	gulafsha="Moni";
+	is="is";
+	best="best";
+	comment=gulafsha + is + best;
+	System.out.println(comment);
+	}
+}
