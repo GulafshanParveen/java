6f3b286be14ab3fb91d0345673c50155cbd2ca05
Gulafshan Parveen <64768393+GulafshanParveen@users.noreply.github.com>
2020-05-28 17:33:10 +0530

    Use of Instance

---
+++ Instanceof.java
@@ -0,0 +1,8 @@
+class Instanceof{
+public static void main(String[] args){
+	String test="Gulafsha";
+	boolean result;
+	result=test instanceof String;
+	System.out.println("Instance of String=" +result);
+	}
+}
