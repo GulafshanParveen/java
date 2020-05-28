cb78045a4f14448c88431cbe12e71a3f87114355
Gulafshan Parveen <64768393+GulafshanParveen@users.noreply.github.com>
2020-05-28 17:44:47 +0530

    Implementation of ternary operator

---
+++ TernaryOperator.java
@@ -0,0 +1,8 @@
+class TernaryOperator{
+	public static void main(String[] args){
+	int feburary=29;
+	String result;
+	result=(feburary==28) ? "not a leap year" : "leap year";
+	System.out.println("result=" +result);
+	}
+}
